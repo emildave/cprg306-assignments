2ca6eda1ffb0537f7178911f099e8cdb03c9307b
Emil Cabutotan <113380544+emildave@users.noreply.github.com>
2024-09-11 13:07:33 -0600

    Add Week 2 Assignments
    Added Week 2 assignment files

---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+export default function StudentInfo() {
+  return (
+    <main>
+      <h1>Emil Cabutotan</h1>
+      <p><Link href="https://github.com/emildave">GitHub</Link></p>
+    </main>
+  );
+}

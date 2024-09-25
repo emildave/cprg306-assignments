72d3f5b330dd0212bec3f10522c694a7741529dc
Emil Cabutotan <113380544+emildave@users.noreply.github.com>
2024-09-24 22:12:01 -0600

    update week 3
    updated week 3 styling

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -1,8 +1,8 @@
 export default function Item(props) {
     return (
         <main>
-            <section className='m-2 bg-violet-100 p-3'>
-            <h2 className='text-xl font-bold text-violet-900'>{props.name}</h2>
+            <section className='m-2 bg-primary p-3'>
+            <h2 className='text-xl font-bold text-text'>{props.name}</h2>
             <p>Buy {props.quantity} in {props.category}</p>
             </section>
         </main>

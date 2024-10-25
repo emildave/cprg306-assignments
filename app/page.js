cc81b4f2c4113ef2d989aa199d4555f699b0086c
Emil Cabutotan <113380544+emildave@users.noreply.github.com>
2024-10-24 18:28:43 -0600

    completed week 7
    completed week 7 lab and fixed an error in app/page.js

--- app/page.js
+++ app/page.js
@@ -1,8 +1,11 @@
 import Link from 'next/link';
 
 export default function Page() {
+  
+  const button = "px-3 py-2 text-violet-900 text-lg hover:text-lg bg-violet-200 w-fit hover:bg-violet-300 font-bold rounded-xl";
+
   return (
-    <main class name='flex flex-col gap-5'>
+    <main className='flex flex-col gap-5'>
       <h1 className="text-violet-900 text-4xl font-bold">
         CPRG 306: Web Development 2 - Assignments
       </h1>
@@ -12,9 +15,8 @@ export default function Page() {
         <Link href='./week-4' className={button}>Week 4</Link>
         <Link href='./week-5' className={button}>Week 5</Link>
         <Link href='./week-6' className={button}>Week 6</Link>
+        <Link href='./week-7' className={button}>Week 7</Link>
       </ul>
     </main>
   );
 }
-
-const button = "px-3 py-2 text-violet-900 text-lg hover:text-lg bg-violet-200 w-fit hover:bg-violet-300 font-bold rounded-xl";

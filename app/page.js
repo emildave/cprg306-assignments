52eec447ac3a07808f35f370684905dfb8d3ee24
Emil Cabutotan <113380544+emildave@users.noreply.github.com>
2024-11-07 13:44:12 -0700

    completed week 9 assignment

--- app/page.js
+++ app/page.js
@@ -17,6 +17,7 @@ export default function Page() {
         <Link href='./week-6' className={button}>Week 6</Link>
         <Link href='./week-7' className={button}>Week 7</Link>
         <Link href='./week-8' className={button}>Week 8</Link>
+        <Link href='./week-9' className={button}>Week 9</Link>
       </ul>
     </main>
   );
